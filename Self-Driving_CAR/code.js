5444e62fa17082aafac9741331ea592bc46541cf
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-06-14 16:55:23 +0530

    Eod commit
    Added sensor script.. and done with the road script

--- Self-Driving_CAR/code.js
+++ Self-Driving_CAR/code.js
@@ -7,13 +7,19 @@ const road = new Road(canvas.width/2,canvas.width*0.9)
 const car = new Car(road.getLaneCenter(3),200,30,50);
 //car.draw(ctx);
 
-animate();
+animate();  
 function animate()
 {
     car.update();
     canvas.height=window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*.7);
+
     // canvas.width=window.innerWidth;
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
