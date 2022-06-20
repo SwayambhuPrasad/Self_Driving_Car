7383726ae275e618715a4ba5ab83f79ae4cad58c
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-06-20 17:01:19 +0530

    Eod commit

--- Self-Driving_CAR/car.js
+++ Self-Driving_CAR/car.js
@@ -18,8 +18,31 @@ class Car{
     }
     update(roadBorder)
     {
-     this.#move();   
+     this.#move();
+     this.polygon=this.#createPolygon();
      this.sensor.update(roadBorder);
+    }
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan(this.width,this.height);
+        points.push({
+            x:this.x+Math.sin(this.angle-alpha)*rad,
+            y:this.y+Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y+Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x+Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        return points;
     }
     #move(){
         if(this.controls.forward)
@@ -60,20 +83,15 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-        ctx.beginPath();
-        ctx.rect(
-        -this.width/2,
-        -this.height/2,
-        this.width,
-        this.height);
+       ctx.beginPath();
+       ctx.moveTo(this.polygon[0].x,this.polygon[0].y)
+       for(let i=0;i<this.polygon.length;i++){
+           ctx.lineTo(this.polygon[i].x,this.polygon[i].y)
+       }
         ctx.fillStyle= "#7E1925";
         ctx.fill();
         ctx.strokeStyle = "#500810";
         ctx.stroke();
-        ctx.restore();
 
         this.sensor.draw(ctx);
     }    
--- Self-Driving_CAR/road.js
+++ Self-Driving_CAR/road.js
@@ -1,8 +1,8 @@
 class Road{
-    constructor(x,width,lanecount=4){
+    constructor(x,width,laneCount=4){
         this.x=x;
         this.width=width;
-        this.lanecount=lanecount;
+        this.laneCount=laneCount;
 
         this.left=x-width/2;
         this.right=x+width/2;
@@ -11,36 +11,53 @@ class Road{
         this.top=-infinity;
         this.bottom=infinity;
 
+        const topLeft={x:this.left,y:this.top};
+        const topRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
+        this.borders=[
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ];
+
     }
     getLaneCenter(laneIndex)
     {
-        const laneWidth=this.width/this.lanecount;
-        return this.left+laneWidth/2+laneIndex*laneWidth;
+        const laneWidth=this.width/this.laneCount;
+        return this.left+laneWidth/2+
+        Math.min(laneIndex,this.laneCount-1)*laneWidth;
     }
     draw(ctx){
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        for(let i=0;i<=this.lanecount;i++)
+        for(let i=0;i<=this.laneCount;i++)
         {
            const x=lerp(
                this.left,
                this.right,
-               i/this.lanecount
+               i/this.laneCount
            );
-           if(i>0 && i<this.lanecount)
+           if(i>0 && i<this.laneCount)
            ctx.setLineDash([20,20])
            else
            ctx.setLineDash([]);
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
-            // if(i==0 || i==this.lanecount)
+            // if(i==0 || i==this.laneCount)
             // ctx.strokeStyle="black";
             // else
             // ctx.strokeStyle="white";
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        });
     }
 
 }
